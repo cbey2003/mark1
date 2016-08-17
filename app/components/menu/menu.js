13eb5d6628cdfd564f5c607d960fb4ad7f0b8bfa
Virgilio Cometa <virgilio.cometa@mobilelive.ca>
2016-08-17 11:00:25 -0400

    Initial commit

---
+++ app/components/menu/menu.js
@@ -0,0 +1,34 @@
+'use strict';
+
+angular.module('myApp.menu', [])
+
+
+.config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
+    $locationProvider.hashPrefix('');
+
+    $routeProvider.otherwise({redirectTo: '/view1'});
+}])
+
+.directive('ngMenu', function() {
+    return {
+        scope : {
+            id: '@menuId',
+            primary: '@isPrimary',
+            path : '@menuSrc'
+        },
+
+        templateUrl: 'components/menu/menu.html',
+
+        controller: ['$scope','$http', function ngMenuController($scope, $http){
+            var self = this;
+
+            $scope.menutype = $scope.primary=='true'?'primary':'';
+
+            $http.get($scope.path).then(function(response) {
+                $scope.menuItems = response.data;
+            });
+        }]
+    }
+});
+
+
