0f308bf62ff0b9dbb60405db5b41f0760243fb28
Virgilio Cometa <virgilio.cometa@mobilelive.ca>
2016-08-25 15:56:04 -0400

    currency format

--- app/filters/formatCurrency/formatCurrency.js
+++ app/filters/formatCurrency/formatCurrency.js
@@ -7,7 +7,7 @@ angular.module('myApp.formatCurrency', [])
             var rem = ( (amount % 1).toFixed(2) )*100,
                 floor = amount - (amount % 1),
                 dec = (rem != 0 )?rem:'00',
-                output = "<div class='currency'><span>"+currencySymbol +"</span><span>"+ floor + "</span><sup>."+dec+"</sup><sub>"+perUnit+"</sub></div>";
+                output = "<div class='currency'><span class='currency-symbol'>"+currencySymbol +"</span><span>"+ floor + ".</span><sup>"+dec+"</sup><sub>"+perUnit+"</sub></div>";
             return String(output);
         };
     });
